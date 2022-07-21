87a9978b571b699d3485fabf58b194466aff869b
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-07-21 23:53:16 +0800

    Converting into String

---
+++ PDF Resume/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF_Resume
+{
+    internal class Pdf
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public string Objectives { get; set; }
+        public string Skill1 { get; set; }
+        public string Skill1Description { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill2Description { get; set; }
+        public string Skill3 { get; set; }
+        public string Skill3Description { get; set; }
+
+        public string College { get; set; }
+        public string CollegeCourse { get; set; }
+        public string CollegeSchoolYear { get; set; }
+        public string SHS { get; set; }
+        public string SHSStrand { get; set; }
+        public string SHSSchoolYear { get; set; }
+        public string HS { get; set; }
+        public string HSSchoolYear { get; set; }
+
+        public string Recognition1 { get; set; }
+        public string Recognition2 { get; set; }
+        public string Recognition3 { get; set; }
+        public string Recognition4 { get; set; }
+
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Facebook { get; set; }
+
+    }
+}
--- PDF Resume/Form1.cs
+++ PDF Resume/Form1.cs
@@ -12,10 +12,44 @@ namespace PDF_Resume
         public Form1()
         {
             InitializeComponent();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
         }
 
         private void buttonConvert_Click(object sender, EventArgs e)
         {
+            String pdf_file = (@"C: \Users\HP\Documents\Outputs\MyResume.json");
+            String Json = File.ReadAllText(pdf_file);
+            Pdf resume = JsonSerializer.Deserialize<Pdf>(Json)!;
+
+            String Name = resume.Name;
+            String Position = resume.Position;
+
+            String Objectives = resume.Objectives;
+            String Skill1 = resume.Skill1;
+            String Skill1Description = resume.Skill1Description;
+            String Skill2 = resume.Skill2;
+            String Skill2Description = resume.Skill2Description;
+            String Skill3 = resume.Skill3;
+            String Skill3Description = resume.Skill3Description;
+
+            String College = resume.College;
+            String CollegeCourse = resume.CollegeCourse;
+            String CollegeSchoolYear = resume.CollegeSchoolYear;
+            String SHS = resume.SHS;
+            String SHSStrand = resume.SHSStrand;
+            String SHSSchoolYear = resume.SHSSchoolYear;
+            String HS = resume.HS;
+            String HSSchoolYear = resume.HSSchoolYear;
+
+            String Recognition1 = resume.Recognition1;
+            String Recognition2 = resume.Recognition2;
+            String Recognition3 = resume.Recognition3;
+            String Recognition4 = resume.Recognition4;
+
+            String Address = resume.Address;
+            String ContactNumber = resume.ContactNumber;
+            String EmailAddress = resume.EmailAddress;
+            String Facebook = resume.Facebook;
 
         }
     }
