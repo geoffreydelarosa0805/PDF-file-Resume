b5bc26697cdc8c48e7c14c76f2c78c04ed15be2a
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-07-22 23:27:35 +0800

    Deserialize

--- PDF Resume/Form1.Designer.cs
+++ PDF Resume/Form1.Designer.cs
@@ -29,32 +29,90 @@
         private void InitializeComponent()
         {
             this.buttonConvert = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonConvert
             // 
-            this.buttonConvert.Location = new System.Drawing.Point(171, 109);
+            this.buttonConvert.Location = new System.Drawing.Point(29, 100);
             this.buttonConvert.Name = "buttonConvert";
-            this.buttonConvert.Size = new System.Drawing.Size(98, 42);
+            this.buttonConvert.Size = new System.Drawing.Size(73, 24);
             this.buttonConvert.TabIndex = 0;
-            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.Text = "Read File";
             this.buttonConvert.UseVisualStyleBackColor = true;
             this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(29, 53);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(102, 15);
+            this.label.TabIndex = 1;
+            this.label.Text = "Input the Json File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(29, 180);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(554, 450);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Location = new System.Drawing.Point(29, 140);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(81, 15);
+            this.labelInformation.TabIndex = 5;
+            this.labelInformation.Text = "Informations :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 295);
+            this.ClientSize = new System.Drawing.Size(614, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label);
             this.Controls.Add(this.buttonConvert);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button buttonConvert;
+        private Label label;
+        private TextBox textBox1;
+        private TextBox textBoxName;
+        private Label labelInformation;
+        private Button button1;
     }
 }
--- PDF Resume/Form1.cs
+++ PDF Resume/Form1.cs
@@ -1,9 +1,4 @@
-using System.Text.Json;
-using PdfSharp;
-using PdfSharp.Drawing;
-using PdfSharp.Pdf;
-using System.Diagnostics;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace PDF_Resume
 {
@@ -12,84 +7,15 @@ namespace PDF_Resume
         public Form1()
         {
             InitializeComponent();
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
         }
 
         private void buttonConvert_Click(object sender, EventArgs e)
         {
-            String pdf_file = (@"C: \Users\HP\Documents\Outputs\MyResume.json");
-            String Json = File.ReadAllText(pdf_file);
-            Pdf resume = JsonSerializer.Deserialize<Pdf>(Json)!;
-
-            String Name = resume.Name;
-            String Position = resume.Position;
-
-            // Objectives
-            String Objectives = resume.Objectives;
-
-            // Skills
-            String Skill1 = resume.Skill1;
-            String Skill1Description = resume.Skill1Description;
-            String Skill2 = resume.Skill2;
-            String Skill2Description = resume.Skill2Description;
-            String Skill3 = resume.Skill3;
-            String Skill3Description = resume.Skill3Description;
-
-            // Educational Background
-            String College = resume.College;
-            String CollegeCourse = resume.CollegeCourse;
-            String CollegeSchoolYear = resume.CollegeSchoolYear;
-            String SHS = resume.SHS;
-            String SHSStrand = resume.SHSStrand;
-            String SHSSchoolYear = resume.SHSSchoolYear;
-            String HS = resume.HS;
-            String HSSchoolYear = resume.HSSchoolYear;
-
-            // Achievements
-            String Recognition1 = resume.Recognition1;
-            String Recognition2 = resume.Recognition2;
-            String Recognition3 = resume.Recognition3;
-            String Recognition4 = resume.Recognition4;
-
-            // Contacts
-            String Address = resume.Address;
-            String ContactNumber = resume.ContactNumber;
-            String EmailAddress = resume.EmailAddress;
-            String Facebook = resume.Facebook;
-
-            using (SaveFileDialog save = new SaveFileDialog())
-            {
-                save.InitialDirectory = (@"C: \Users\HP\Documents\Outputs\Resume");
-                save.FileName = Name + ".pdf";
-                save.Filter = "PDF|*.pdf";
-
-                if (save.ShowDialog() == DialogResult.OK)
-                {
-                    PdfDocument pdfdocument = new PdfDocument();
-                    PdfPage pagedocument = pdfdocument.AddPage();
-                    pdfdocument.Info.Title = "APPLICATION RESUME";
-
-                    XGraphics grp = XGraphics.FromPdfPage(pagedocument);
-
-                    XFont afont = new XFont("Cambria", 30, XFontStyle.Bold);
-                    XFont bfont = new XFont("Bookman Old Style", 20, XFontStyle.Bold);
-                    XFont cfont = new XFont("Cambria", 12, XFontStyle.Regular);
-
-                    int down = 150;
-                    int left = 50;
-
-                    {
-                        String picture = (@"C: \Users\HP\Pictures\Saved Pictures\Dela-Rosa.jpg");
-                        XImage image = XImage.FromFile(picture);
+            var JsonFile = @"C:\Users\HP\Documents\My Resume";
+            String JsonInfo = File.ReadAllText(JsonFile);
+            Pdf resume = JsonConvert.DeserializeObject<Pdf>(JsonInfo);
 
 
-                        grp.DrawString(Name, afont, XBrushes.Black, new XRect(0, 0, pagedocument.Width, pagedocument.Height), XStringFormats.Center);
-
-                    }
-
-                    pdfdocument.Save(save.FileName);
-                }
-            }
         }
     }
 }
