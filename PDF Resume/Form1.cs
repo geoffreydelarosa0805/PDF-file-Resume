c793895b642439bfd400bffe496404a4f63668f5
Geoffrey Dela Rosa <delarosageoff@gmail.com>
2022-07-22 23:09:17 +0800

    Continuation

--- PDF Resume/Form1.cs
+++ PDF Resume/Form1.cs
@@ -24,7 +24,10 @@ namespace PDF_Resume
             String Name = resume.Name;
             String Position = resume.Position;
 
+            // Objectives
             String Objectives = resume.Objectives;
+
+            // Skills
             String Skill1 = resume.Skill1;
             String Skill1Description = resume.Skill1Description;
             String Skill2 = resume.Skill2;
@@ -32,6 +35,7 @@ namespace PDF_Resume
             String Skill3 = resume.Skill3;
             String Skill3Description = resume.Skill3Description;
 
+            // Educational Background
             String College = resume.College;
             String CollegeCourse = resume.CollegeCourse;
             String CollegeSchoolYear = resume.CollegeSchoolYear;
@@ -41,16 +45,51 @@ namespace PDF_Resume
             String HS = resume.HS;
             String HSSchoolYear = resume.HSSchoolYear;
 
+            // Achievements
             String Recognition1 = resume.Recognition1;
             String Recognition2 = resume.Recognition2;
             String Recognition3 = resume.Recognition3;
             String Recognition4 = resume.Recognition4;
 
+            // Contacts
             String Address = resume.Address;
             String ContactNumber = resume.ContactNumber;
             String EmailAddress = resume.EmailAddress;
             String Facebook = resume.Facebook;
 
+            using (SaveFileDialog save = new SaveFileDialog())
+            {
+                save.InitialDirectory = (@"C: \Users\HP\Documents\Outputs\Resume");
+                save.FileName = Name + ".pdf";
+                save.Filter = "PDF|*.pdf";
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    PdfDocument pdfdocument = new PdfDocument();
+                    PdfPage pagedocument = pdfdocument.AddPage();
+                    pdfdocument.Info.Title = "APPLICATION RESUME";
+
+                    XGraphics grp = XGraphics.FromPdfPage(pagedocument);
+
+                    XFont afont = new XFont("Cambria", 30, XFontStyle.Bold);
+                    XFont bfont = new XFont("Bookman Old Style", 20, XFontStyle.Bold);
+                    XFont cfont = new XFont("Cambria", 12, XFontStyle.Regular);
+
+                    int down = 150;
+                    int left = 50;
+
+                    {
+                        String picture = (@"C: \Users\HP\Pictures\Saved Pictures\Dela-Rosa.jpg");
+                        XImage image = XImage.FromFile(picture);
+
+
+                        grp.DrawString(Name, afont, XBrushes.Black, new XRect(0, 0, pagedocument.Width, pagedocument.Height), XStringFormats.Center);
+
+                    }
+
+                    pdfdocument.Save(save.FileName);
+                }
+            }
         }
     }
 }
